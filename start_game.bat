0f9207a11b2d53ad6387fbdd1ecab07050502a4c
ck1020974 <ck1020974@gmail.com>
2025-12-10 00:36:17 +0800

    Initial commit

---
+++ start_game.bat
@@ -0,0 +1,9 @@
+@echo off
+echo 正在啟動政壇鬼抓人伺服器...
+echo 請勿關閉此視窗，否則遊戲將無法載入資料。
+
+:: 開啟瀏覽器
+start "" "http://localhost:8000/game.html?cache_bust=ui_refine_v29"
+
+:: 啟動 Python 伺服器
+python -m http.server 8000
