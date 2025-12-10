324c36b7cdf11ea1a9289bf342dc4d26ce73e61c
ck1020974 <ck1020974@gmail.com>
2025-12-10 20:49:53 +0800

    UI Optimization: Scale interface to 80% and improve mobile layout for investigation modal

--- start_game.bat
+++ start_game.bat
@@ -3,7 +3,7 @@ echo 正在啟動政壇鬼抓人伺服器...
 echo 請勿關閉此視窗，否則遊戲將無法載入資料。
 
 :: 開啟瀏覽器
-start "" "http://localhost:8000/game.html?cache_bust=ui_refine_v29"
+start "" "http://localhost:8000/index.html?cache_bust=ui_refine_v30"
 
 :: 啟動 Python 伺服器
 python -m http.server 8000
